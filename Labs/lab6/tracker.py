10487b0566008441b578bfc60a3b8e6c50d4872b
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-12-01 19:47:32 -0800

    PING drafted

--- Labs/lab6/tracker.py
+++ Labs/lab6/tracker.py
@@ -8,6 +8,8 @@ import bencodepy
 import socket
 import threading
 
+import time # for timestamp
+
 class Tracker:
     """
     This class contains methods that provide implementations to support trackerless peers
@@ -39,7 +41,7 @@ class Tracker:
 
     def broadcast(self, message, self_broadcast_enabled=False):
         try:
-            print(f'Broadcast: {message}')
+            # print(f'Broadcast: {message}')
             encoded_message = self.encode(message)
             self.udp_socket.sendto(encoded_message, ('<broadcast>', self.DHT_PORT))
             print('Broadcasting...')
@@ -102,26 +104,15 @@ class Tracker:
         TODO: implement the ping method. 
         :return:
         """
-        message = {}
-        # if(y == 'q'):
-        message['t'] = t
-        message['y'] = y
-        message['q'] = 'ping'
-        message['a'] = a
-        # message['r'] = r
+        # Send Ping
+        message = {
+            't': t,
+            'y': y,
+            'q': 'ping',
+            'a': a
+        }
         self.broadcast(message)
-        # elif(y == 'r'):
-        #     message = {}
-        #     message['t'] = t
-        #     message['y'] = y
-        #     message['r'] = r
-        #     node = self.getNode(r['id'])
-        #     print(node)
-        #     self.send_udp_message(message, node['ip'], node['port'])
-            # self.
-            # message['nodes'] = self.get_peers()
-            # node = self.find_entry_in_routing_table_from_node_id(a['id'])
-            # self.send(query, node['ip_address'], node['port']);
+
 
     def getNode(self, id):
         for node in self._routing_table:
@@ -135,9 +126,15 @@ class Tracker:
         :return:
         """
         message = {"t":t, "y":y, "q":"find_node", "a": a}
-        self.broadcast(message)
+        try:
+            for node in self._routing_table:
+                ip = node['ip_address']
+                port = node['port']
+                print(f'find_node --> {message} | {ip} | {port}')
 
-        
+                self.send_udp_message(message, ip, port) 
+        except Exception as err:
+            print(f'Failed to send find_node query --> {err}')
 
     def get_peers(self, t, y, a=None, r=None):
         """
@@ -153,38 +150,67 @@ class Tracker:
         """
         pass
 
+    def addNode(self, nodeID, ip_address, port, info_hash, last_changed):
+        # will story a list of dictionaries representing entries in the routing table
+        # dictionaries stored here are in the following form
+        # {'nodeID': '<the node id is a SHA1 hash of the ip_address and port of the server node and a random uuid>',
+        #  'ip_address': '<the ip address of the node>', 'port': '<the port number of the node',
+        #  'info_hash': '<the info hash from the torrent file>', last_changed': 'timestamp'}
+        node = {
+            'nodeID': nodeID,
+            'ip_address': ip_address,
+            'port': port,
+            'info_hash': info_hash,
+            'last_changed': last_changed
+        }
+        self._routing_table.append(node)
+        print(f'Added Node: {node}')
+
     def process_query(self, query, ip_sender, port_sender):
         """
         TODO: process an incoming query from a node
         :return: the response
         """
-        print(f'Process: {query}')
+        print(f'Process: {query} | {ip_sender} | {port_sender}')
         
         # QUERY
         if(query['y'] == 'q'):
-            # PING
+            # PING QUERY
             if(query['q'] == 'ping'):
-                print('\nPING')
-                node = {}
-                node['id'] = query['a']['id']
-                node['ip'] = ip_sender
-                node['port'] = port_sender
-                self._routing_table.append(node)
-                # self.ping(t='aa', y='r', r={"id":"mnopqrstuvwxyz123456"})
-                self.ping(query['t'], query['y'], query['a'])
-                return
+                print('PING')
+
+                # Add node to routing table
+                try:
+                    info_hash = self._torrent.info_hash()
+                    timestamp = time.time()
+                    self.addNode(query['a']['id'], ip_sender, port_sender, info_hash, timestamp)
+                except Exception as err:
+                    print(f'Failed to add node to routing table --> {err}')
+
+                # Send response to ping
+                try:
+                    message = {
+                        't': 'aa',
+                        'y': 'r',
+                        'r': {
+                            "id":"0123456789abcdefghij", 
+                            "nodes": "def456..."
+                        }
+                    }
+                    self.send_udp_message(message, ip_sender, port_sender)
+                except Exception as err:
+                    print(f'Failed to send ping response --> {err}')
+
+            # FIND_NODE QUERY     
+            elif(query['q'] == 'find_node'):
+                print('FIND_NODE')
+                
+
         
         # RESPONSE
-        if(query['y'] == 'r'):
-            print('\nRESPONSE')
-            node = {}
-            node['id'] = query['r']['id']
-            node['ip'] = ip_sender
-            node['port'] = port_sender
-            self._routing_table.append(node)
-            print(self._routing_table)
-            return
-                    
+        elif(query['y'] == 'r'):
+            print('RESPONSE')
+
 
     def send_response(self):
         """
@@ -208,15 +234,36 @@ class Tracker:
         if self._is_announce:
             threading.Thread(target=self.broadcast_listener).start()
             if start_with_broadcast:
-                # message = "Anyone Listening"
-                # self.broadcast(message, True)
-                query = {}
-                query['t'] = 'aa'
-                query['y'] = 'q'
-                query['q'] = 'ping'
-                query['a'] = {"id":"abcdefghij0123456789"}
-                self.process_query(query, node['ip'], node['port'])
-                # self.ping(message['t'], message['y'], message['a'])
+                # PING
+                query = {
+                    't': 'aa',
+                    'y': 'q',
+                    'q': 'ping',
+                    'a': {
+                        "id":"abcdefghij0123456789"
+                    }
+                }
+                self.ping(t=query['t'], y=query['y'], a=query['a'])
+                del query
+
+                time.sleep(1)
+                
+                # FIND_NODE
+                query = {
+                    't': 'aa',
+                    'y': 'q',
+                    'q': 'find_node',
+                    'a': {
+                        'id': 'abcdefghij0123456789',
+                        'target': 'mnopqrstuvwxyz123456'
+                    }
+                }
+                self.find_node(t=query['t'], y=query['y'], a=query['a'])
+                del query
+
+                time.sleep(1)
+
+                print('DONE')
         else:
             print('This tracker does not support DHT protocol')
 
